b7943bbbe114ece5f76216b8803c64ce2eaf032a
asif <muhammedasifs321@gmail.com>
2025-09-08 04:49:05 +0530

    dakygsd

--- edusetin_app/urls.py
+++ edusetin_app/urls.py
@@ -9,6 +9,36 @@ urlpatterns = [
 
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
+
+    # edusetin
+    path('test-preparation-details/', views.test_preparation_details, name='test_preparation_details'),
+    path('visa-guidance-details/', views.visa_guidance_details, name='visa_guidance_details'),
+    path('application-process-details/', views.application_process_details, name='application_process_details'),
+    path('course-selection-details/', views.course_selection_details, name='course_selection_details'),
+    path('career-counselling-details/', views.career_counselling_details, name='career_counselling_details'),
+
+    path('united-kingdom/', views.united_kingdom_details, name='united_kingdom_details'),
+    path('germany/', views.germany_details, name='germany_details'),
+    path('italy/', views.italy_details, name='italy_details'),
+    path('dubai/', views.dubai_details, name='dubai_details'),
+    path('kyrgyzstan/', views.kyrgyzstan_details, name='kyrgyzstan_details'),
+
+    path('harvard-university/', views.harvard_details, name='harvard_details'),
+    path('oxford-university/', views.oxford_details, name='oxford_details'),
+    path('bologna-university/', views.bologna_details, name='bologna_details'),
+    path('sapienza-university/', views.sapienza_details, name='sapienza_details'),
+    path('ludwig-university/', views.ludwig_details, name='ludwig_details'),
+    path('asian-medical-institute/', views.asian_medical_institute_details, name='asian_medical_institute_details'),
+    path('kyrgyz-state-academy/', views.kyrgyz_state_academy_details, name='kyrgyz_state_academy_details'),
+
+    path('business-management/', views.business_management, name='business_management_details'),
+    path('engineering/', views.engineering, name='engineering_details'),
+    path('health-and-medicine/', views.health_and_medicine, name='health_medicine_details'),
+
+
+
+
+
     path('blogs/', views.blogs, name='blogs'),
     path('blog_details/', views.blog_details, name='blog_details'),
     path('contact-us/', views.contact_us, name='contact-us'),
--- edusetin_app/views.py
+++ edusetin_app/views.py
@@ -7,6 +7,76 @@ def index(request):
 def about(request):
     return render(request, 'about.html')
 
+def application_process_details(request):
+    return render(request, 'edusetin/application-process-details.html')
+
+def career_counselling_details(request):
+    return render(request, 'edusetin/career-counselling-details.html')
+
+def course_selection_details(request):
+    return render(request, 'edusetin/course-selection-details.html')
+
+def test_preparation_details(request):
+    return render(request, 'edusetin/test-preparation-details.html')
+
+def visa_guidance_details(request):
+    return render(request, 'edusetin/visa-guidance-details.html')
+
+
+def united_kingdom_details(request):
+    return render(request, 'edusetin/united-kingdom-details.html')
+
+def germany_details(request):
+    return render(request, 'edusetin/germany-details.html')
+
+def italy_details(request):
+    return render(request, 'edusetin/italy-details.html')
+
+def dubai_details(request):
+    return render(request, 'edusetin/dubai-details.html')
+
+def kyrgyzstan_details(request):
+    return render(request, 'edusetin/kyrgyzstan-details.html')
+
+# Harvard University
+def harvard_details(request):
+    return render(request, 'edusetin/harvard-university-details.html')
+
+# University of Oxford
+def oxford_details(request):
+    return render(request, 'edusetin/oxford-university-details.html')
+
+# University of Bologna
+def bologna_details(request):
+    return render(request, 'edusetin/bologna-university-details.html')
+
+# Sapienza University of Rome
+def sapienza_details(request):
+    return render(request, 'edusetin/sapienza-university-details.html')
+
+# Ludwig Maximilian University of Munich
+def ludwig_details(request):
+    return render(request, 'edusetin/ludwig-university-details.html')
+
+# Asian Medical Institute
+def asian_medical_institute_details(request):
+    return render(request, 'edusetin/asian-medical-institute-details.html')
+
+# Kyrgyz State Medical Academy
+def kyrgyz_state_academy_details(request):
+    return render(request, 'edusetin/kyrgyz-state-academy-details.html')
+
+def business_management(request):
+    return render(request, 'edusetin/business-management.html')
+
+def engineering(request):
+    return render(request, 'edusetin/engineering.html')
+
+def health_and_medicine(request):
+    return render(request, 'edusetin/health-and-medicine.html')
+
+
+
 def blogs(request):
     return render(request, 'blogs.html')
 
